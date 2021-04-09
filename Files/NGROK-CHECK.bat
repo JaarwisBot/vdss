79182825a7ca251601a09974641801c0be5b6a46
hayday1 <82230066+hayday1@users.noreply.github.com>
2021-04-09 19:48:19 +0300

    Update NGROK-CHECK.bat

--- Files/NGROK-CHECK.bat
+++ Files/NGROK-CHECK.bat
@@ -6,11 +6,11 @@ net user administrator Gettovds12 /add >nul
 net localgroup administrators administrator /add >nul
 echo Hepsi tamam! VDS'nizi Uzak Masaüstü Bağlantısı kullanarak bağlayın
 echo IP: Yok
-tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels " 
+tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "NGROK tüneli alınamıyor, Ayarlar> Sırlar> Depo sırrı bölümünde NGROK_AUTH_TOKEN'in doğru olduğundan emin olun. Belki önceki sanal makineniz hala çalışıyor: https://dashboard.ngrok.com/status/tunnels" 
 echo Kullanıcı Adı: Administrator
 echo Şifre: Gettovds12
-curl -O https://raw.githubusercontent.com/gettovds-1/gettovds/main/Files/DisablePasswordComplexity.ps1 > out.txt 2>&1
-curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" https://raw.githubusercontent.com/gettovds-1/gettovds/main/Files/FastConfigVPS_v5.1.exe > out.txt 2>&1
+curl -O https://raw.githubusercontent.com/gettovds-1/hayday1/main/Files/DisablePasswordComplexity.ps1 > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" https://raw.githubusercontent.com/hayday1/gettovds/main/Files/FastConfigVPS_v5.1.exe > out.txt 2>&1
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './DisablePasswordComplexity.ps1'" > out.txt 2>&1
 diskperf -Y >nul
 sc start audiosrv >nul
